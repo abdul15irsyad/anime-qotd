962f4b1b395b619687a37f04bb6c8d6349f8ad76
Irsyad Abdul Hamid Darussalam <90173897@bri.co.id>
2025-09-22 20:50:44 +0700

    feat: add character images and show information to character data

--- src/scripts/utils/characters.ts
+++ src/scripts/utils/characters.ts
@@ -7,15 +7,24 @@ export const generateCharacters = () => {
   const quotes = JSON.parse(
     readFileSync('./src/datas/quotes.json', { encoding: 'utf-8' }),
   ) as IYurippeQuote[];
-  const characters = [...new Set(quotes.map(({ character }) => character))];
+  const characterUniqueNames = [
+    ...new Set(quotes.map(({ character }) => character)),
+  ];
+  const characters = characterUniqueNames.map((name) => {
+    const quote = quotes.find(({ character }) => character === name)!;
+    return {
+      name: quote.character,
+      show: quote.show,
+    };
+  });
   console.log(`total characters: ${characters.length} data`);
 
   const filePath = './src/datas/characters.json';
   const fileContent = JSON.stringify(
     characters
       .map((character) => ({
-        name: character,
-        image: `${slugify(character, { lower: true, strict: true })}.jpg`,
+        ...character,
+        image: `${slugify(character.name, { lower: true, strict: true })}.jpg`,
       }))
       .sort((a, b) => (a.name < b.name ? -1 : 1)),
     null,
@@ -28,12 +37,13 @@ export const generateCharacters = () => {
 export const checkCharacterImages = () => {
   const characters = JSON.parse(
     readFileSync('./src/datas/characters.json', { encoding: 'utf-8' }),
-  ) as { name: string; image: string }[];
+  ) as { name: string; show: string; image: string }[];
 
-  const charactersWithImage = characters.map(({ name, image }) => ({
+  const charactersWithImage = characters.map(({ name, show, image }) => ({
     name,
     image,
     haveImage: existsSync(`./public/characters/${image}`),
+    show,
   }));
   console.log(charactersWithImage.filter(({ haveImage }) => !haveImage).length);
   console.table(
